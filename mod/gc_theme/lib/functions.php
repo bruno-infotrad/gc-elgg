528fde5556133aa7aff3871290d9c249a51d9023
Bruno <ti@infotrad.ca>
2015-07-30 08:51:12 -0400

    Add alphabetical sorting to parent page dropwdown when adding/editing a page

--- mod/gc_theme/lib/functions.php
+++ mod/gc_theme/lib/functions.php
@@ -26,3 +26,60 @@ function gc_get_group_members($group_guid, $limit = 10, $offset = 0, $site_guid 
                 'site_guid' => $site_guid
         ));
 }
+
+function gc_pages_get_navigation_tree($container) {
+	if (!elgg_instanceof($container)) {
+		return;
+	}
+	$dbprefix = elgg_get_config("dbprefix");
+
+	$top_pages = new ElggBatch('elgg_get_entities', array(
+		'type' => 'object',
+		'subtype' => 'page_top',
+		'container_guid' => $container->getGUID(),
+		'joins' => array("JOIN " . $dbprefix . "objects_entity o ON e.guid=o.guid"),
+		'order_by' => 'o.title asc',
+		'limit' => false,
+	));
+
+	/* @var ElggBatch $top_pages Batch of top level pages */
+
+	$tree = array();
+	$depths = array();
+
+	foreach ($top_pages as $page) {
+		$tree[] = array(
+			'guid' => $page->getGUID(),
+			'title' => $page->title,
+			'url' => $page->getURL(),
+			'depth' => 0,
+		);
+		$depths[$page->guid] = 0;
+
+		$stack = array();
+		array_push($stack, $page);
+		while (count($stack) > 0) {
+			$parent = array_pop($stack);
+			$children = new ElggBatch('elgg_get_entities_from_metadata', array(
+				'type' => 'object',
+				'subtype' => 'page',
+				'metadata_name' => 'parent_guid',
+				'metadata_value' => $parent->getGUID(),
+				'limit' => false,
+			));
+
+			foreach ($children as $child) {
+				$tree[] = array(
+					'guid' => $child->getGUID(),
+					'title' => $child->title,
+					'url' => $child->getURL(),
+					'parent_guid' => $parent->getGUID(),
+					'depth' => $depths[$parent->guid] + 1,
+				);
+				$depths[$child->guid] = $depths[$parent->guid] + 1;
+				array_push($stack, $child);
+			}
+		}
+	}
+	return $tree;
+}
--- mod/gc_theme/views/default/pages/input/parent.php
+++ mod/gc_theme/views/default/pages/input/parent.php
@@ -8,15 +8,13 @@
  * @uses $vars['entity']         Optional. The child entity (uses container_guid)
  */
 
-elgg_load_library('elgg:pages');
-
 if (empty($vars['entity'])) {
 	$container = elgg_get_page_owner_entity();
 } else {
 	$container = $vars['entity']->getContainerEntity();
 }
 
-$pages = pages_get_navigation_tree($container);
+$pages = gc_pages_get_navigation_tree($container);
 $options = array();
 if (elgg_is_admin_logged_in() || roles_has_role(elgg_get_logged_in_user_entity(),'im_admin')) {
 	$options['0'] = elgg_echo('gc_theme:top_level_page');
