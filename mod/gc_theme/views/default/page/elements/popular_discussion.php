d4377a2eb586a4bf13a4ec02ea7a48b1de8588a0
Bruno <ti@infotrad.ca>
2015-09-16 16:27:07 -0400

    Strip html tags in most active view to avoid formatting issues (missing html tags)

--- mod/gc_theme/views/default/page/elements/popular_discussion.php
+++ mod/gc_theme/views/default/page/elements/popular_discussion.php
@@ -6,6 +6,7 @@ if ($discussion->title) {
 } else {
 	$text = $discussion->description;
 }
+$text=strip_tags($text);
 $text = (strlen($text) > 203) ? substr($text,0,200).'...' : $text;
 //$text = var_export($discussion,true).' '.$text;
 $icon = elgg_view_entity_icon($owner, 'small');
@@ -13,4 +14,4 @@ $summary = elgg_view('output/url', array(
 		'href' => $discussion->getURL(),
 		'text' => $text,
 	));
-echo '<div class="elgg-image-block clearfix"><div class="elgg-image">'.$icon.'</div><div class="elgg-body sidebar-discussion-item">'.$summary.'</div><div class="gc-small-date">'.elgg_view_friendly_time($discussion->time_created).'</div></div>';
+echo '<div class="elgg-image-block clearfix"><div class="elgg-image">'.$icon.'</div><div class="elgg-body sidebar-discussion-item">'.$summary.'<div class="gc-small-date">'.elgg_view_friendly_time($discussion->time_created).'</div></div></div>';
