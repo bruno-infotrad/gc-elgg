2e419f8b4a7a17ee09eca02707b862412c11b826
bruno-infotrad <ti@infotrad.ca>
2015-09-22 11:03:59 -0400

    Fix base_url for group wall

--- mod/gc_theme/views/default/gc_theme/ajax/group_wall.php
+++ mod/gc_theme/views/default/gc_theme/ajax/group_wall.php
@@ -4,7 +4,6 @@ $user = elgg_get_logged_in_user_entity();
 $offset = get_input('offset');
 $options['offset'] = $offset;
 $base_url = get_input('base_url');
-$options['base_url'] = $base_url;
 
 if (!$group || !elgg_instanceof($group, 'group')) {
 	register_error(elgg_echo('groups:notfound'));
@@ -24,6 +23,7 @@ if (group_gatekeeper(false)) {
 		'wheres' => array("(e.container_guid = $group->guid OR rv.object_guid = $group->guid) AND rv.action_type != 'vote' AND rv.action_type != 'join'"),
 	);
 	$options['body_class'] = 'new-feed';
+	$options['base_url'] = $base_url;
 	$activity = elgg_list_river($options);
 	
 	if (!$activity) {
