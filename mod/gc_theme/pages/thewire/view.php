775a9dc9bcf63c8019f176d1def0b4cce699ffc5
Bruno <ti@infotrad.ca>
2015-10-22 13:41:19 -0400

    More work on fix permission issue whereby non-group members could comment on an item when accessing it in the group or on the exec content page (object view instead of river view)

--- mod/gc_theme/pages/thewire/view.php
+++ mod/gc_theme/pages/thewire/view.php
@@ -23,14 +23,22 @@ $title = $thewire->title;
 
 if (elgg_instanceof($container, 'group')) {
 	elgg_push_breadcrumb($container->name, "thewire_group/group/$container->guid/all");
+	if ($container->isMember(elgg_get_logged_in_user_entity())) {
+		$comments = elgg_view_comments($thewire);
+	} else {
+		$comments = elgg_view_comments($thewire,FALSE);
+	}
 } else {
 	elgg_push_breadcrumb($container->name, "thewire/owner/$container->username");
+	$comments = elgg_view_comments($thewire);
 }
 //pages_prepare_parent_breadcrumbs($thewire);
 elgg_push_breadcrumb($title);
 
 $content = elgg_view_entity($thewire, array('full_view' => true));
-$content .= elgg_view_comments($thewire);
+//Test if group member, if not do not allow to comment
+
+$content .= $comments;
 $body = elgg_view_layout('content', array(
 	'filter' => '',
 	'content' => $content,
