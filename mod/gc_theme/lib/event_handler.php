e72491dbe53bb83a952c43a881aa3b58b2d5cf29
Bruno <ti@infotrad.ca>
2016-01-05 07:17:00 -0500

    Remove event handler for colleague creation notification

--- mod/gc_theme/lib/event_handler.php
+++ mod/gc_theme/lib/event_handler.php
@@ -584,18 +584,6 @@ function gc_theme_pagesetup_handler() {
 	}
 }
 
-function gc_relationship_notification_hook($event, $type, $object) {
-
-        $user_one = get_entity($object->guid_one);
-        $user_two = get_entity($object->guid_two);
-
-        return notify_user($object->guid_two,
-                        $object->guid_one,
-                        elgg_echo('friend:newfriend:subject', array($user_one->name)),
-                        elgg_echo("friend:newfriend:body", array($user_one->name, $user_one->getURL(), elgg_get_site_url()))
-        );
-}
-
 function ad2elgg_user_update($event, $type, $user) {
 	if (elgg_is_active_plugin('ntlm_sso')) {
 		if(!empty($user) && ($user instanceof ElggUser)){
