488e9f400750659dabaa9936c36de548af8d0b0d
Bruno <ti@infotrad.ca>
2016-03-01 15:55:57 -0500

    Add line to specify semicolon as default separator in export_member

--- mod/gc_theme/pages/groups/export_members.php
+++ mod/gc_theme/pages/groups/export_members.php
@@ -4,6 +4,7 @@ if ($group_guid) {
 	header("Content-Type: text/csv");
 	header("Content-Disposition: Attachment; filename=export.csv");
 	header('Pragma: public');
+	echo "sep=;\r\n";
 	$members = gc_get_group_members($group_guid, $limit = 0, $offset = 0, $site_guid = 0, $count = false);
 	foreach ($members as $member) {
 		echo $member->username.';'.$member->name.';'.$member->email."\r\n";
