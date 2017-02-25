19ebe36913d56ff7b011faccd995115abba93e68
bruno-infotrad <ti@infotrad.ca>
2017-02-25 08:59:04 -0500

    Fix missing base_url in options (for next-to-first clicks on more)

--- mod/gc_theme/views/default/gc_theme/ajax/discussion.php
+++ mod/gc_theme/views/default/gc_theme/ajax/discussion.php
@@ -3,16 +3,16 @@ $page_type = get_input('page_type');
 $offset = get_input('offset');
 $container_guid = get_input('group_guid');
 $base_url = get_input('base_url');
-$options['base_url'] = $base_url;
 elgg_group_gatekeeper();
 $group = get_entity($container_guid);
 if (!elgg_instanceof($group, 'group')) {
 	forward('', '404');
 }
 $options = array(
+	'base_url' => $base_url,
 	'type' => 'object',
 	'subtype' => 'groupforumtopic',
-	'limit' => max(10, elgg_get_config('default_limit')),
+	'limit' => max(20, elgg_get_config('default_limit')),
 	'order_by' => 'e.last_action desc',
 	'container_guid' => $container_guid,
 	'full_view' => false,
