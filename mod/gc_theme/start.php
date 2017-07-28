1fdbac15260afa6fda76c4a3add713368a45042e
bruno-infotrad <ti@infotrad.ca>
2017-07-28 07:14:07 -0400

    Double quotes for newline

--- mod/gc_theme/start.php
+++ mod/gc_theme/start.php
@@ -399,9 +399,9 @@ function gc_classifieds_cleanup() {
 	}
 	foreach($ads as $ad) {
 		if ($ad->delete()) {
-			echo "Deleted Ad ".$ad->title.' '.elgg_view_friendly_time($ad->time_updated).'\n';
+			echo "Deleted Ad ".$ad->title.' '.elgg_view_friendly_time($ad->time_updated)."\r\n";
 		} else {
-			echo "Could not delete Ad ".$ad->title.' '.elgg_view_friendly_time($ad->time_updated).'\n';
+			echo "Could not delete Ad ".$ad->title.' '.elgg_view_friendly_time($ad->time_updated)."\n";
 		}
 	}
 }
