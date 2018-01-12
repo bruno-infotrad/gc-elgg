ad3ae4bf5922b936320c2911d222e81f26e45d55
bruno-infotrad <ti@infotrad.ca>
2018-01-12 07:08:01 -0500

    Fix bug where every group title was burgundy (not only the read only ones)

--- mod/gc_theme/views/default/page/elements/group_title.php
+++ mod/gc_theme/views/default/page/elements/group_title.php
@@ -3,11 +3,10 @@ if (isset($vars['header'])) {
 	echo $vars['header'];
 	return true;
 }
-
 if (isset($vars['readonly'])) {
 	echo "<h2 id='readonly'>".elgg_echo('gc_theme:groups:readonly:message')."</h2>";
 }
-$class = " id='readonly2'";
+if ($vars['readonly'] == ' readonly') { $class = " id='readonly2'";}
 if (isset($vars['class'])) {
 	$class = " class=\"{$vars['class']}\"";
 }
