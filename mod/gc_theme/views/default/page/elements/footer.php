9d752b8de4f4aba0f2bfdab9459872ab30e1168b
bruno-infotrad <ti@infotrad.ca>
2018-02-02 15:47:57 -0500

    Add js to redirect the two groups which are part of the Agora/GCconnex migration pilot

--- mod/gc_theme/views/default/page/elements/footer.php
+++ mod/gc_theme/views/default/page/elements/footer.php
@@ -4,3 +4,13 @@ if ($_SESSION['language'] == 'fr') {
 } else {
 	include(elgg_get_plugins_path()."gc_theme/views/default/gc_theme/footer_en.php");
 }
+?>
+<script>
+$(document).ready(function(){
+	if (window.location.href.match(/\/59163\//)){
+		window.location.replace("https://gcconnex.gc.ca/groups/profile/34520708/englobal-affairs-canada-choir-without-bordersfrchorale-sans-frontiu00e8res-daffaires-mondiales-canada");
+	} else if (window.location.href.match(/\/181546\//)){
+		window.location.replace("https://gcconnex.gc.ca/groups/profile/34575612/englobal-affairs-canada-u2013-sii-cornerfraffaires-mondiales-canada-u2013-coin-sii");
+	}
+});
+</script>
